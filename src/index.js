51ea142185d95bb1b929cfb4ea009e3108e6358b
fay-ehi <faithgoddey2002@gmail.com>
2024-07-11 16:24:35 +0100

    Added API to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input-form");
-
-  let cityElement = document.querySelector("h1");
-  cityElement.innerHTML = searchInput.value;
+  let city = searchInput.value;
+  let apiKey = "8d8ob1t4a343b0f13b62705ad423c092";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(getWeather);
 }
 
+function getWeather(response) {
+  let cityElement = document.querySelector("h1");
+  cityElement.innerHTML = response.data.city;
+}
 let searchElement = document.querySelector(".searchForm");
 searchElement.addEventListener("submit", searchCity);
