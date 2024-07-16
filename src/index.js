a7e7f2123969f5dcf8d25f2ac4da52018313aef7
fay-ehi <faithgoddey2002@gmail.com>
2024-07-16 17:58:07 +0100

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 function getWeather(response) {
   let cityElement = document.querySelector("h1");
   cityElement.innerHTML = response.data.city;
+
   let temperatureElement = document.querySelector("#temperature-number");
   let temperature = Math.round(response.data.temperature.current);
   temperatureElement.innerHTML = temperature;
+
   let weatherDescription = document.querySelector("#weather-description");
   weatherDescription.innerHTML = response.data.condition.description;
 
@@ -12,7 +14,12 @@ function getWeather(response) {
 
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
+
+  let date = new Date(response.data.time * 1000);
+  let dateElement = document.querySelector(".date-time");
+  dateElement.innerHTML = formatDate(date);
 }
+
 function searchCity(city) {
   let apiKey = "8d8ob1t4a343b0f13b62705ad423c092";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -28,4 +35,23 @@ function handleSearch(event) {
 let searchElement = document.querySelector(".searchForm");
 searchElement.addEventListener("submit", handleSearch);
 
-searchCity("chicago");
+searchCity("australia");
+
+function formatDate(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hour}:${minutes}`;
+}
